199dd8a19b7ddd00ba9617d91a7a725eda0aae63
Morax-xyc <xyc@jbhdf.xyz>
2022-02-26 17:57:15 +0800

    revise the method of getting env

--- ssh_login.sh
+++ ssh_login.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-TELEGRAMUSERID="<telegram user id>"
-TOKEN="<bot token>"
+source /etc/tgnotice/.config/telegramuserid
+source /etc/tgnotice/.config/bot-token
 
 for i in "${TELEGRAMUSERID[@]}"
 do
